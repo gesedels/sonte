95610251f312e9b41296bcb0541e3d146ca0655f
Stephen Malone <mail@gesedels.com>
2025-07-22 10:08:10 +1000

    Added tools/path package.

---
+++ sonte/tools/path/path.go
@@ -0,0 +1,55 @@
+// Package path implements file path manipulation functions.
+package path
+
+import (
+	"path/filepath"
+	"strings"
+)
+
+// Base returns a file path's name with the extension.
+func Base(orig string) string {
+	return filepath.Base(orig)
+}
+
+// Dire returns a file path's parent directory.
+func Dire(orig string) string {
+	return filepath.Dir(orig)
+}
+
+// Extn returns a file path's extension with a leading dot.
+func Extn(orig string) string {
+	base := filepath.Base(orig)
+	if clip := strings.Index(base, "."); clip != -1 {
+		return base[clip:]
+	}
+
+	return ""
+}
+
+// Glob returns all file paths in a directory matching an extension.
+func Glob(dire, extn string) []string {
+	glob := filepath.Join(dire, "*"+extn)
+	origs, _ := filepath.Glob(glob)
+	return origs
+}
+
+// Join returns a joined file path from a directory, name and extension.
+func Join(dire, name, extn string) string {
+	return filepath.Join(dire, name+extn)
+}
+
+// Match returns true if a file path's name contains a substring.
+func Match(orig, text string) bool {
+	name := Name(orig)
+	return strings.Contains(name, text)
+}
+
+// Name returns a file path's name without the extension.
+func Name(orig string) string {
+	base := filepath.Base(orig)
+	if clip := strings.Index(base, "."); clip != -1 {
+		return base[:clip]
+	}
+
+	return base
+}
---
+++ sonte/tools/path/path_test.go
@@ -0,0 +1,77 @@
+package path
+
+import (
+	"path/filepath"
+	"testing"
+
+	"github.com/gesedels/sonte/sonte/tools/test"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBase(t *testing.T) {
+	// success
+	base := Base("/dire/name.extn")
+	assert.Equal(t, "name.extn", base)
+}
+
+func TestDire(t *testing.T) {
+	// success
+	dire := Dire("/dire/name.extn")
+	assert.Equal(t, "/dire", dire)
+}
+
+func TestExtn(t *testing.T) {
+	// success - real extension
+	extn := Extn("/dire/name.extn")
+	assert.Equal(t, ".extn", extn)
+
+	// success - bare extension
+	extn = Extn("/dire/name.")
+	assert.Equal(t, ".", extn)
+
+	// success - no extension
+	extn = Extn("/dire/name")
+	assert.Equal(t, "", extn)
+}
+
+func TestGlob(t *testing.T) {
+	// setup
+	dire := test.MockDire(t)
+
+	// success
+	origs := Glob(dire, ".extn")
+	assert.Equal(t, []string{
+		filepath.Join(dire, "alpha.extn"),
+		filepath.Join(dire, "bravo.extn"),
+	}, origs)
+}
+
+func TestJoin(t *testing.T) {
+	// success
+	dest := Join("/dire", "name", ".extn")
+	assert.Equal(t, "/dire/name.extn", dest)
+}
+
+func TestMatch(t *testing.T) {
+	// success - true
+	ok := Match("/dire/name.extn", "nam")
+	assert.True(t, ok)
+
+	// success - false
+	ok = Match("/dire/name.extn", "nope")
+	assert.False(t, ok)
+}
+
+func TestName(t *testing.T) {
+	// success - real extension
+	name := Name("/dire/name.extn")
+	assert.Equal(t, "name", name)
+
+	// success - bare extension
+	name = Name("/dire/name.")
+	assert.Equal(t, "name", name)
+
+	// success - no extension
+	name = Name("/dire/name")
+	assert.Equal(t, "name", name)
+}
